d30dc2208ecc27aec8c4c312d7dedce4ad904052
Yann Sionneau <yann@sionneau.net>
2018-08-30 21:46:34 +0200

    squash migrations + render account balance graph

--- mescomptes/urls.py
+++ mescomptes/urls.py
@@ -4,5 +4,8 @@ from mescomptes import views
 
 urlpatterns = [
     url(r'^$', index, name="index"),
+    url('^compte/(?P<compte_num>[0-9]*)/get_balance_evolution$', views.get_balance_evolution,
+        name="get_balance_evolution"),
+    url('^compte/(?P<compte_nom>.*)$', views.compte_view, name="view_compte"),
     url(r'^set_categories$', views.set_categories, name="set_categories")
 ]
--- mescomptes/views.py
+++ mescomptes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
-from django.http import Http404
+from django.http import Http404, JsonResponse
 
 from mescomptes.forms import SetCategoriesForm
-from mescomptes.models import Inscription
+from mescomptes.models import Inscription, Compte
 
 
 def index(request):
@@ -22,3 +22,42 @@ def set_categories(request):
         if form.is_valid():
             pass
     return render(request, 'mescomptes/set_categories.html', {'form': form})
+
+
+def compte_view(request, compte_nom):
+    try:
+        compte = Compte.objects.get(name=compte_nom)
+    except Compte.DoesNotExist:
+        raise Http404(_("This account does not exist"))
+    return render(request, 'mescomptes/compte.html', {'compte': compte})
+
+
+def get_balance_evolution(request, compte_num):
+    if request.method == "GET":
+        try:
+            compte = Compte.objects.get(pk=compte_num)
+        except Compte.DoesNotExist:
+            raise Http404(_("This account does not exist"))
+        data = dict({
+            "data": [{
+                    'x': [],
+                    'y': []
+                 }],
+            "layout": {
+                "title": "Compte: {}".format(compte.name)
+            }
+        })
+        d = data['data'][0]
+        d['x'].append(compte.opening_date)
+        d['y'].append(compte.opening_balance)
+        balance = compte.opening_balance
+        for inscription in compte.inscription_set.all().order_by('date'):
+            if inscription.debit:
+                balance = balance - inscription.debit
+            if inscription.credit:
+                balance = balance + inscription.credit
+            d['x'].append(inscription.date)
+            d['y'].append(balance)
+        return JsonResponse(data)
+    else:
+        raise Http404(_("Only GET method is allowed"))  # should be http 405 error code
